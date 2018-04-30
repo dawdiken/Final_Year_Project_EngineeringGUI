137751362fbc70a02424193c6196adc33f23a254
DAVID KENNY - Student <G00070718@gmit.ie>
2018-04-30 12:57:29 +0100

    refactor

--- src/NewWorkGui.java
+++ src/NewWorkGui.java
@@ -1,4 +1,6 @@
 
+import net.miginfocom.swing.MigLayout;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.crypto.Cipher;
@@ -13,22 +15,25 @@ public class NewWorkGui extends JPanel
         PropertyChangeListener {
     private EngineeringDataAccess database;
     private ProgressMonitor progressMonitor;
-    private JButton selectWorksOrder;
+    private JButton selectWorksOrder, saveNewJob;
     private JTextArea taskOutput;
     private JTextField JobNumField, batchQtyField;
-    private JLabel jobNumLabel, customerLabel, batchQtyLabel,departmentLabel, sopLabel;
+    private JLabel jobNumLabel, customerLabel, batchQtyLabel,departmentLabel, sopLabel,partNameLabel,custLabel;
     private JPanel dataPanel;
     private Worker worker;
     private JComboBox<String> partNames, sopNames, depNames;
+    private JFrame frame;
 
 
     public NewWorkGui(NewJobEntry job) {
        // super();
 
 //        JFrame frame = new JFrame("Login Form");
+        dataPanel = new JPanel(new MigLayout("wrap 4","[grow][grow]","[][grow][][][]"));
         //dataPanel = new JPanel(new net.miginfocom.swing.MigLayout("wrap 4","[grow][grow]","[][grow][][][]"));
-        dataPanel = new JPanel();
+        //dataPanel = new JPanel();
 
+        custLabel=new JLabel("Customer Name: ");
         customerLabel=new JLabel(job.getCustomerName());
 
         jobNumLabel = new JLabel("Job Number: ");
@@ -65,6 +70,9 @@ public class NewWorkGui extends JPanel
         /////////////////////////////////////////////////////////////
         //////////////////////////////////////////////////////////////////////
         //Get the part name
+        partNameLabel = new JLabel("Part Name: ");
+        partNameLabel.setBorder(
+                BorderFactory.createEmptyBorder( 5, 5, 5, 5 ) );
         ArrayList<String> PartNames = new ArrayList<>();
         String[] result = {};
         PartNames = database.findTechDrawing(job.getCustomerName());
@@ -122,21 +130,40 @@ public class NewWorkGui extends JPanel
 
         //////////////////////////////////////////////////////////////////////
         //////////////////////////////////////////////////////////////////////
-        //Get dept
+        //Button
+        saveNewJob = new JButton("Save New Work Order");
+        saveNewJob.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getJobInfo(job);
+            }
+        });
+
+
+
+
+
+
 //        worker = new Worker(job);
 //        worker.addPropertyChangeListener(this);
 //        worker.execute();
 
-        dataPanel.add(customerLabel, "split 2, gap 35,wrap" );
-        dataPanel.add(jobNumLabel, "split 3" );
+
+
+
+        dataPanel.add(custLabel, "" );
+        dataPanel.add(customerLabel, "growx , gap 35,growx,wrap" );
+        dataPanel.add(jobNumLabel, "" );
         dataPanel.add(JobNumField, "growx , gap 35,growx,wrap");
-        dataPanel.add(partNames);
+        dataPanel.add(partNameLabel);
+        dataPanel.add(partNames, "growx , gap 35,growx,wrap");
         dataPanel.add(sopLabel);
-        dataPanel.add(sopNames);
+        dataPanel.add(sopNames, "growx , gap 35,growx,wrap");
         dataPanel.add(departmentLabel);
-        dataPanel.add(depNames);
+        dataPanel.add(depNames, "growx , gap 35,growx,wrap");
         dataPanel.add(batchQtyLabel);
-        dataPanel.add(batchQtyField);
+        dataPanel.add(batchQtyField, "growx , gap 35,growx,wrap");
+        dataPanel.add(saveNewJob, "growx , gap 35,growx,wrap");
 
 
 
@@ -196,12 +223,11 @@ public class NewWorkGui extends JPanel
 
     public void createAndShowGUI(NewJobEntry job) {
         //Create and set up the window.
-        JFrame frame = new JFrame("New Work Order Menu:");
+        frame = new JFrame("New Work Order Menu:");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         JComponent newContentPane = new NewWorkGui(job);
         newContentPane.setOpaque(true);
         frame.setContentPane(newContentPane);
-
         frame.pack();
         frame.setVisible(true);
     }
@@ -228,54 +254,79 @@ public class NewWorkGui extends JPanel
         }
     }
 
-    private void ZippFolder(String pathToFolder, String saveAs){
-        ZipUtils appZip = new ZipUtils();
-        appZip.generateFileList(new File(pathToFolder),pathToFolder);
-        appZip.zipIt("C:\\EDHRHOME\\ArchivedFiles\\"+saveAs,pathToFolder );//default location where zippe file will always be
-    }
-
-    private void EncryptFolder(String saveAs){
-        String key = "This is a secret";
-        File encryptedFile = new File("C:\\EDHRHOME\\ArchivedFiles\\"+saveAs+".encrypted");
-        File inputFile = new File("C:\\EDHRHOME\\ArchivedFiles\\"+saveAs);
-        EncryptFiles encryptMyFolder = new EncryptFiles();
-        encryptMyFolder.fileProcessor(Cipher.ENCRYPT_MODE,key,inputFile,encryptedFile);
-    }
-
-    private void StoreInCloud(String saveAs){
-        List<String> list = new ArrayList<String>();
-        try{
-            CloudStorageHelper.uploadFile("longtermstorageedhr", "C:\\EDHRHOME\\ArchivedFiles\\"+saveAs+".encrypted");
-        }
-        catch(Exception ee){
-            ee.printStackTrace();
-        }
-    }
-
-    private List<String> CloudContents(){
-        List<String> list = new ArrayList<String>();
-        try{
-            list = CloudStorageHelper.listBucket("longtermstorageedhr");
-        }
-        catch(Exception ee){
-            ee.printStackTrace();
-        }
-        return list;
-    }
-
-//    private String getOption( String fieldName )
-//    {
-//        JComboBox field =
-//                ( JComboBox ) fields.get( fieldName );
-//        Object obj = field.getSelectedItem();
-//        System.out.println("obj= " +obj);
-//        String name = obj.toString();
-////        String name = field.getSelectedItem().toString();
-////        String text = mySpinner.getSelectedItem().toString();
-//
-//        return field.getSelectedItem().toString();
-////        return name;
+//    private void ZippFolder(String pathToFolder, String saveAs){
+//        ZipUtils appZip = new ZipUtils();
+//        appZip.generateFileList(new File(pathToFolder),pathToFolder);
+//        appZip.zipIt("C:\\EDHRHOME\\ArchivedFiles\\"+saveAs,pathToFolder );//default location where zippe file will always be
 //    }
+
+//    private void EncryptFolder(String saveAs){
+//        String key = "This is a secret";
+//        File encryptedFile = new File("C:\\EDHRHOME\\ArchivedFiles\\"+saveAs+".encrypted");
+//        File inputFile = new File("C:\\EDHRHOME\\ArchivedFiles\\"+saveAs);
+//        EncryptFiles encryptMyFolder = new EncryptFiles();
+//        encryptMyFolder.fileProcessor(Cipher.ENCRYPT_MODE,key,inputFile,encryptedFile);
+//    }
+
+//    private void StoreInCloud(String saveAs){
+//        List<String> list = new ArrayList<String>();
+//        try{
+//            CloudStorageHelper.uploadFile("longtermstorageedhr", "C:\\EDHRHOME\\ArchivedFiles\\"+saveAs+".encrypted");
+//        }
+//        catch(Exception ee){
+//            ee.printStackTrace();
+//        }
+//    }
+
+//    private List<String> CloudContents(){
+//        List<String> list = new ArrayList<String>();
+//        try{
+//            list = CloudStorageHelper.listBucket("longtermstorageedhr");
+//        }
+//        catch(Exception ee){
+//            ee.printStackTrace();
+//        }
+//        return list;
+//    }
+
+
+    private void saveNewJobToDB (NewJobEntry job){
+        //boolean success;
+        try {
+            boolean success = database.saveJob(job);
+            if(success){
+                JOptionPane.showMessageDialog( dataPanel, "Success!\nNew works order saved to the database",
+                        "Save successful", JOptionPane.PLAIN_MESSAGE );
+                int jobnum = Integer.parseInt(job.getJobNumber()) + 1 ;
+                String newValue = Integer.toString(jobnum);
+                JobNumField.setText(newValue);
+                batchQtyField.setText("");
+            }
+        }
+        // detect problems saving job
+        catch ( DataAccessException exception ) {
+            JOptionPane.showMessageDialog( dataPanel, exception,
+                    "Save failed", JOptionPane.ERROR_MESSAGE );
+            exception.printStackTrace();
+        }
+    }
+
+    private void getJobInfo(NewJobEntry job)
+    {
+        job.setCustomerName( job.getCustomerName());
+        job.setJobNumber( JobNumField.getText() );
+        job.setPartName( partNames.getSelectedItem().toString().replace(".pdf", "").replace(".jpg", "").replace(".jpeg", "").replace(".png", ""));
+        job.setTechniaclDrawing( partNames.getSelectedItem().toString() );
+        job.setPartSop( sopNames.getSelectedItem().toString() );
+        job.setDepartment(depNames.getSelectedItem().toString() );
+        job.setBatchQty( batchQtyField.getText() );
+        job.setActive("false");
+        job.setQtyMade(0);
+        job.setQtyScrap(0);
+        job.setBatchNumber(1234);
+        job.setMachineID( 1 );
+        saveNewJobToDB(job);
+    }
 
     private class Worker extends SwingWorker< List<String>, String> {
         //get paths and filename to save as sent into the string worker
@@ -312,21 +363,110 @@ public class NewWorkGui extends JPanel
                 //setProgress(25);
                 i = 25;
                 process(i);
-                EncryptFolder(saveAs);
+//                EncryptFolder(saveAs);
                 i = 45;
                 process(i);
                 //setProgress(45);
                 i = 70;
                 process(i);
                 //setProgress(70);
-                StoreInCloud(saveAs);
+//                StoreInCloud(saveAs);
                 i = 80;
                 process(i);
                 //setProgress(85);
                 i = 100;
                 process(i);
                 //setProgress(100);
-                list = CloudContents();
+//                list = CloudContents();
+
+            }
+            catch (Exception ee) {
+                ee.printStackTrace();
+            }
+            return list;
+        }
+
+        //        @Override
+        public void process(Integer chunks)
+        {
+            // define what the event dispatch thread
+            // will do with the intermediate results received
+            // while the thread is executing
+            int val = chunks;
+            setProgress(val);
+            System.out.println("val"+val);
+        }
+
+        @Override
+        public void done() {
+            try{
+                List<String> list = get();
+                taskOutput.append("\nArchive Contents = \n");
+                for (String number : list) {
+                    taskOutput.append(number+ "\n");
+                }
+            }
+            catch (Exception ee){
+                ee.printStackTrace();
+            }
+            Toolkit.getDefaultToolkit().beep();
+            selectWorksOrder.setEnabled(true);
+        }
+        private String pathToFolder;
+        private String saveAs;
+        private NewJobEntry job;
+    }
+
+    private class Worker2 extends SwingWorker< List<String>, String> {
+        //get paths and filename to save as sent into the string worker
+        private Worker2(NewJobEntry job) {
+            this.job = job;
+        }
+
+        @Override
+        public  List<String> doInBackground() {
+            setProgress(0);
+            Integer jobNum = 0;
+            List<String> list = new ArrayList<String>();
+            try {
+                //setProgress(5);
+                int i = 5;
+                process(i);
+                try {
+                    database = new DataBaseAccess();
+                }
+                // detect problems with database connection
+                catch ( Exception exception ) {
+                    exception.printStackTrace();
+                    System.exit( 1 );
+                }
+                System.out.println("job numeber search");
+                try{
+                    jobNum = database.findMaxJobId();
+                }
+                catch (Exception ee) {
+                    ee.printStackTrace();
+                }
+                jobNum++;
+
+                //setProgress(25);
+                i = 25;
+                process(i);
+//                EncryptFolder(saveAs);
+                i = 45;
+                process(i);
+                //setProgress(45);
+                i = 70;
+                process(i);
+                //setProgress(70);
+//                StoreInCloud(saveAs);
+                i = 80;
+                process(i);
+                //setProgress(85);
+                i = 100;
+                process(i);
+                //setProgress(100);
+//                list = CloudContents();
 
             }
             catch (Exception ee) {
